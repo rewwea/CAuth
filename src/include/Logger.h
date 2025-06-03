597369d0c806f30453d0f8d7a0ce77555545c57d
Artem Andronov <AVAndronov10@gmail.com>
2025-06-03 09:40:57 +0500

    Refactor Logger class to include log levels and improve logging format

--- src/include/Logger.h
+++ src/include/Logger.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <ctime>
 
@@ -7,9 +8,26 @@ using namespace std;
 
 class Logger {
 public:
-    static void log(const string& message) {
+    enum Level { INFO, WARNING, ERROR };
+
+    static void log(const string& msg, Level lvl = INFO) {
+        ofstream logFile("log.txt", ios::app);
+        string prefix;
+
+        switch (lvl) {
+            case INFO:    prefix = "[INFO] "; break;
+            case WARNING: prefix = "[WARN] "; break;
+            case ERROR:   prefix = "[ERROR] "; break;
+        }
+
         time_t now = time(nullptr);
-        cout << "[" << ctime(&now);
-        cout << "] " << message << endl;
+        string timestamp = string(ctime(&now));
+        timestamp.pop_back(); // убрать \n
+
+        string fullMsg = prefix + timestamp + " - " + msg;
+
+        cout << fullMsg << endl;
+        logFile << fullMsg << endl;
+        logFile.close();
     }
 };
